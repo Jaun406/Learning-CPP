78cad1f01a254798ca8be406d57da0ba8bb9039f
Jaun Minnaar <152258555+Jaun406@users.noreply.github.com>
2025-11-24 23:09:06 +0200

    Add files via upload

---
+++ 1. Diving In/1_1.myFirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Input and output streams to get input and output
+
+// main function (entry point of C++ program)
+int main() {
+    //Statements run from top to bottom
+    std::cout << "Hello World!" << std::endl;
+
+    for(int i = 1; i <= 10; i++){
+        std::cout <<"Jaun Minnaar"<< std::endl;
+    }
+
+    return 0;
+}
